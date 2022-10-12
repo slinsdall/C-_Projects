19b90cba9beadd0b460323ef22ff13c8177d702c
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-10-12 14:13:03 -0700

    Add files via upload

---
+++ Basic_C#_Programs/Achme Accounting/Achme Accounting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Achme_Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\n Rememeber, we're \"Accounting\" on you!");
+            Console.ReadLine();
+            Console.Read();
+        }
+    }
+}
